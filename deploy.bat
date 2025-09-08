1b1748a30ae01edac904ce0d5f6e2885a0051894
hellenakim1234 <hellenak68@gmail.com>
2025-09-08 12:54:26 +0900

    Auto update

--- deploy.bat
+++ deploy.bat
@@ -1,9 +1,9 @@
-@echo off
-chcp 65001 >nul
-echo Starting Auto Deploy...
-git add .
-git commit -m "Auto update"
-git push
-echo Deploy Complete!
-echo Check your website in a few minutes.
-pause
+@echo off 
+chcp 65001 >nul 
+echo Starting Auto Deploy... 
+git add . 
+git commit -m "Auto update" 
+git push 
+echo Deploy Complete! 
+echo Check your website in a few minutes. 
+pause 
